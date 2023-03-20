97152b823a3f9994998842714a52066639698ed5
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2023-03-20 10:32:15 -0300

    feat: Checking player movements

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,6 +1,6 @@
 defmodule ExMon do
   alias ExMon.{Game, Player}
-  alias ExMon.Game.Status
+  alias ExMon.Game.{Actions, Status}
 
   @computer_name :Sephiroth
 
@@ -12,5 +12,9 @@ defmodule ExMon do
     @computer_name |> create_player(:punch, :kick, :heal) |> ExMon.Game.start(player)
 
     Status.print_round_message()
+  end
+
+  def make_move(move) do
+    Actions.fetch_move(move)
   end
 end
