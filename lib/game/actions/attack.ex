f435c4f158866e38ab9f734910a70d4173e0d3f9
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2023-05-02 20:19:35 -0300

    feat: Changing turns and states of the game

--- lib/game/actions/attack.ex
+++ lib/game/actions/attack.ex
@@ -1,5 +1,6 @@
 defmodule ExMon.Game.Actions.Attack do
   alias ExMon.Game
+  alias ExMon.Game.Status
 
   @move_avg_power 18..25
   @move_rnd_power 10..35
@@ -11,7 +12,7 @@ defmodule ExMon.Game.Actions.Attack do
     |> Game.fetch_player()
     |> Map.get(:life)
     |> calculate_total_life(damage)
-    |> update_opponent_life(opponent)
+    |> update_opponent_life(opponent, damage)
   end
 
   defp calculate_power(:move_avg), do: Enum.random(@move_avg_power)
@@ -20,11 +21,18 @@ defmodule ExMon.Game.Actions.Attack do
   defp calculate_total_life(life, damage) when life - damage < 0, do: 0
   defp calculate_total_life(life, damage), do: life - damage
 
-  defp update_opponent_life(remaining_life, opponent) do
-    opponent |> Game.fetch_player() |> Map.put(:life, remaining_life) |> update_game(opponent)
+  defp update_opponent_life(remaining_life, opponent, damage) do
+    opponent
+    |> Game.fetch_player()
+    |> Map.put(:life, remaining_life)
+    |> update_game(opponent, damage)
   end
 
-  defp update_game(damaged_opponent, opponent) do
-    Game.info() |> Map.put(opponent, damaged_opponent) |> Game.update()
+  defp update_game(damaged_opponent, opponent, damage) do
+    Game.info()
+    |> Map.put(opponent, damaged_opponent)
+    |> Game.update()
+
+    Status.print_move_message(opponent, :attack, damage)
   end
 end
